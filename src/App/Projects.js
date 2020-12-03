ccddec56c03e6ec2902467a6dd90b04a90f4df8f
Emil van Velden <emilvanvelden@gmail.com>
2020-12-03 16:34:50 +0200

    first commit

---
+++ src/App/Projects.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tasklists from './Tasklists'
+
+export default class Projects extends React.Component {
+	render() {
+		let keys = Object.keys(this.props.projects);
+	  	const projectsList = keys.map(key => {
+		return ( 
+			<ul key={this.props.projects[key].id}>
+				<li>
+					{this.props.projects[key].name}
+					<Tasklists tasklists = {
+						this.props.projects[key].tasklists
+					}
+					/>   
+				</li>
+			</ul> )
+		})
+	  return <div>{projectsList}</div>
+	}
+}
